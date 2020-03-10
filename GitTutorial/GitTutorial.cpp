f0939cac0faabc3badb487f88c0efd475166f17f
AaronXue <her8153101@yahoo.com.tw>
2020-03-10 10:41:23 +0800

    Error fix

--- GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial.cpp
@@ -5,10 +5,11 @@ using namespace System;
 bool Guess(int number) {
 }
 int main(array<System::String ^> ^args)
-{   
+{       
     int guess;
-    do{
-std:cin>>guess;
+       do{
+        std::cout << "Choose a number between 1 - 100 :"
+        std:cin>>guess;
     }while(!Guess(guess));
     return 0;
 }
